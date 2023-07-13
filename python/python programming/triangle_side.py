04981a115cd9353cf56a2f71d119a34dbcb871fd
Renukaradhya T B <134044271+Renukaradhya1234@users.noreply.github.com>
2023-07-13 22:17:47 +0530

    Create triangle_side.py

---
+++ python/python programming/triangle_side.py
@@ -0,0 +1,4 @@
+''' WAP to consider the triangle and check whether, the triangle is equilateral or isosceles or scalene '''
+def main():
+  side1, side2, side3, side4 = float(input('Enter the side1: ')), float(input('Enter the side2')), float(input('Enter the side3: ')), float(input('Enter the side4: '))
+  print('Equliteral...') if a==b and b==c else print('Isosceles...') if a==b or b==c or a==c else print('Scalene..')
